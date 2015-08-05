521a5268e0a10e2538f142d11213930de1a155ea
davehernaez <dave.hernaez@yahoo.com>
2015-08-05 11:06:10 +0800

    Added GreenDao(generator) and generated dao classes(temporarily no use)

---
+++ app/src/main/java/com/hernaez/seven_eleven/GreenDao/GreenDaoGenerator.java
@@ -0,0 +1,25 @@
+package com.hernaez.seven_eleven.GreenDao;
+
+
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+/**
+ * Created by TAS on 8/5/2015.
+ */
+public class GreenDaoGenerator {
+    public  static  void main(String[] args) throws  Exception{
+    Schema schema = new Schema(1,"com.hernaez.seven_eleven.model.dataaccesslayer.greendao");
+        Entity entity = schema.addEntity("Order_Table");
+        entity.addIdProperty();
+        entity.addStringProperty("productName");
+        entity.addIntProperty("productQty");
+        entity.addIntProperty("productPrice");
+        entity.addIntProperty("productSubtotal");
+        entity.addStringProperty("productImgPath");
+        String outputDirectory = "app/src/main/java";
+        new DaoGenerator().generateAll(schema, outputDirectory);
+    }
+}
